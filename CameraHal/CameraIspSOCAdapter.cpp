c9803f726b2d3f29c23592dae35d6d9da4551d63
zyh <zyh@rock-chips.com>
2014-04-14 16:00:49 +0800

    add neon code to optimize arm_isp_yuyv_12bit_to_8bit in soc camera

--- CameraHal/CameraIspSOCAdapter.cpp
+++ CameraHal/CameraIspSOCAdapter.cpp
@@ -15,6 +15,11 @@ static volatile int32_t gLogLevel = 0;
 #define LOG_FUNCTION_NAME           LOG1("%s Enter", __FUNCTION__);
 #define LOG_FUNCTION_NAME_EXIT      LOG1("%s Exit ", __FUNCTION__);
 
+/*
+ * zyh neon optimize
+ */
+#define HAVE_ARM_NEON 1
+
 void CameraIspSOCAdapter::setupPreview(int width_sensor,int height_sensor,int preview_w,int preview_h)
 {
     CamEngineWindow_t dcWin;
@@ -31,14 +36,45 @@ extern "C" void arm_isp_yuyv_12bit_to_8bit (int src_w, int src_h,char *srcbuf,ui
     int  *srcint;
     int i = 0;
     int y_size = 0;
-    unsigned int *dst_buf;
+    unsigned int *dst_buf,*debug_buf;
     unsigned int tmp = 0;
     //for test 
 
     y_size = src_w*src_h;
     srcint = ( int*)srcbuf;
     dst_buf = ( int*)srcbuf;
-
+    debug_buf = ( int*)srcbuf;
+#if HAVE_ARM_NEON
+    /*LOGE("---------arm neon %s-----------",__FUNCTION__);
+    for(i=0;i<16;i++) {
+           LOGE("before:0x%x,",*(debug_buf+i));
+    }*/
+    if(ycSequence == ISI_YCSEQ_CBYCRY){
+		asm volatile (
+				"   pld [%[src], %[src_stride], lsl #2]                         \n\t"
+				"   cmp %[n], #8                                                \n\t"
+				"   blt 5f                                                      \n\t"
+				"0: @ 16 pixel swap                                             \n\t"
+				"   vld2.32 {q0,q1} , [%[src]]!  @ q0 = dat0 q1 = dat1          \n\t"
+				"   vshr.u32 q0,q0,#6            @ now q0  -> y1 00 v0 00       \n\t"
+				"   vshr.u32 q1,q1,#6            @ now q1  -> y0 00 u0 00       \n\t"
+				"   vswp q0, q1                  @ now q0 = q1 q1 = q0          \n\t"
+				"   vuzp.u8 q0,q4                @ now d0 = y0u0.. d8  = 00..   \n\t"
+				"   vuzp.u8 q1,q5                @ now d2 = y1v0.. d10 = 00..   \n\t"
+				"   vst2.16 {d0,d2},[%[dst_buf]]!@ now q0  -> dst               \n\t"
+				"   sub %[n], %[n], #8                                          \n\t"
+				"   cmp %[n], #8                                                \n\t"
+				"   bge 0b                                                      \n\t"
+				"5: @ end                                                       \n\t"
+				: [dst_buf] "+r" (dst_buf),[src] "+r" (srcint), [n] "+r" (y_size)
+				: [src_stride] "r" (y_size)
+				: "cc", "memory", "q0", "q1", "q2","q3"
+				);
+		/*for(i=0;i<16;i++) {
+			   LOGE("after:0x%x,",*(debug_buf+i));
+		}*/
+    }
+#else
     for(i=0;i<(y_size>>1);i++) {
 
         if(ycSequence == ISI_YCSEQ_CBYCRY){
@@ -52,6 +88,7 @@ extern "C" void arm_isp_yuyv_12bit_to_8bit (int src_w, int src_h,char *srcbuf,ui
        srcint += 2;
 
     }
+#endif
 }
 
 void CameraIspSOCAdapter::bufferCb( MediaBuffer_t* pMediaBuffer )
