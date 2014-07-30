7283bddac6f21ac287c4e5c97e9b5a5ea70a58af
zyh <zyh@rock-chips.com>
2014-07-30 11:31:08 +0800

    add file for last commit

--- Config/rk32xx_camera.mk
+++ Config/rk32xx_camera.mk
@@ -1,16 +1,16 @@
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3288)
-TARGET_OUT=out/target/product/rk3288
-#hardware=hardware/rk29/camera/Config 
-$(shell cp -rf hardware/rk29/camera/Config/cam_board.xml $(TARGET_OUT)/system/etc/) 
-$(shell cp -rf hardware/rk29/camera/Config/libisp_silicomimageisp_api.so $(TARGET_OUT)/obj/) 
-$(shell cp -rf hardware/rk29/camera/Config/libisp_silicomimageisp_api.so $(TARGET_OUT)/system/lib/) 
-$(shell cp -rf hardware/rk29/camera/Config/../SiliconImage/isi/drv/OV8820/calib/OV8820.xml $(TARGET_OUT)/system/etc/) 
-$(shell cp -rf hardware/rk29/camera/Config/../SiliconImage/isi/drv/OV8825/calib/OV8825.xml $(TARGET_OUT)/system/etc/) 
-$(shell cp -rf hardware/rk29/camera/Config/../SiliconImage/isi/drv/OV8858/calib/OV8858.xml $(TARGET_OUT)/system/etc/) 
-$(shell cp -rf hardware/rk29/camera/Config/../SiliconImage/isi/drv/OV13850/calib/OV13850.xml $(TARGET_OUT)/system/etc/) 
+PRODUCT_COPY_FILES += \
+    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:obj/lib/libisp_silicomimageisp_api.so \
+    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:system/lib/libisp_silicomimageisp_api.so \
+    hardware/rk29/camera/SiliconImage/isi/drv/OV8820/calib/OV8820.xml:system/etc/OV8820.xml \
+    hardware/rk29/camera/SiliconImage/isi/drv/OV8825/calib/OV8825.xml:system/etc/OV8825.xml \
+    hardware/rk29/camera/SiliconImage/isi/drv/OV8858/calib/OV8858.xml:system/etc/OV8858.xml \
+    hardware/rk29/camera/SiliconImage/isi/drv/OV13850/calib/OV13850.xml:system/etc/OV13850.xml \
+    hardware/rk29/camera/Config/cam_board.xml:system/etc/cam_board.xml 
 endif
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk312x)
-$(shell cp -rf hardware/rk29/camera/Config/libisp_silicomimageisp_api.so $(TARGET_OUT)/obj/) 
-$(shell cp -rf hardware/rk29/camera/Config/libisp_silicomimageisp_api.so $(TARGET_OUT)/system/lib/) 
+PRODUCT_COPY_FILES += \
+    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:obj/lib/libisp_silicomimageisp_api.so \
+    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:system/lib/libisp_silicomimageisp_api.so 
 endif
