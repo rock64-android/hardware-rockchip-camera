58df37ef586d7f732d545f49201076a5fc38e6c0
dalon.zhang <dalon.zhang@rock-chips.com>
2014-12-01 11:15:09 +0800

    Libisp split into two files to compatible with kitkat and lollipop

--- Config/Android.mk
+++ Config/Android.mk
@@ -2,28 +2,50 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3288)
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS := libisp_silicomimageisp_api.so
+LOCAL_MODULE := libisp_silicomimageisp_api
 LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+ifeq ($(strip $(PLATFORM_VERSION)),5.0)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_lollipop.so
+else
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_kitkat.so
+endif
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
 
 
 endif
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk312x)
-
-   include $(CLEAR_VARS)
-    LOCAL_PREBUILT_LIBS := libisp_silicomimageisp_api.so
-    LOCAL_MODULE_TAGS := optional
-    include $(BUILD_MULTI_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libisp_silicomimageisp_api
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+ifeq ($(strip $(PLATFORM_VERSION)),5.0)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_lollipop.so
+else
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_kitkat.so
+endif
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
 
 endif
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3036)
-
-   include $(CLEAR_VARS)
-    LOCAL_PREBUILT_LIBS := libisp_silicomimageisp_api.so
-    LOCAL_MODULE_TAGS := optional
-    include $(BUILD_MULTI_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libisp_silicomimageisp_api
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+ifeq ($(strip $(PLATFORM_VERSION)),5.0)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_lollipop.so
+else
+LOCAL_SRC_FILES := $(LOCAL_MODULE)_kitkat.so
+endif
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
 
 endif
 
--- Config/rk32xx_camera.mk
+++ Config/rk32xx_camera.mk
@@ -1,7 +1,13 @@
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3288)
+ifeq ($(strip $(PLATFORM_VERSION)),5.0)
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_lollipop:obj/lib/libisp_silicomimageisp_api
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_lollipop:system/lib/libisp_silicomimageisp_api
+else
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_kitkat:obj/lib/libisp_silicomimageisp_api
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_kitkat:system/lib/libisp_silicomimageisp_api
+endif
+
 PRODUCT_COPY_FILES += \
-    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:obj/lib/libisp_silicomimageisp_api.so \
-    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:system/lib/libisp_silicomimageisp_api.so \
     hardware/rk29/camera/SiliconImage/isi/drv/OV8820/calib/OV8820.xml:system/etc/OV8820.xml \
     hardware/rk29/camera/SiliconImage/isi/drv/OV8825/calib/OV8825.xml:system/etc/OV8825.xml \
     hardware/rk29/camera/SiliconImage/isi/drv/OV8858/calib/OV8858_lens_LG-9569A2.xml:system/etc/OV8858_lens_LG-9569A2.xml \
@@ -13,18 +19,27 @@ PRODUCT_COPY_FILES += \
     hardware/rk29/camera/SiliconImage/isi/drv/OV5648/calib/OV5648_lens_CHT-842B-MD_OTP.xml:system/etc/OV5648_lens_CHT-842B-MD_OTP.xml \
     hardware/rk29/camera/SiliconImage/isi/drv/OV13850/calib/OV13850.xml:system/etc/OV13850.xml \
     hardware/rk29/camera/Config/cam_board.xml:system/etc/cam_board.xml 
+	
 endif
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk312x)
-PRODUCT_COPY_FILES += \
-    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:obj/lib/libisp_silicomimageisp_api.so \
-    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:system/lib/libisp_silicomimageisp_api.so 
+ifeq ($(strip $(PLATFORM_VERSION)),5.0)
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_lollipop:obj/lib/libisp_silicomimageisp_api
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_lollipop:system/lib/libisp_silicomimageisp_api
+else
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_kitkat:obj/lib/libisp_silicomimageisp_api
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_kitkat:system/lib/libisp_silicomimageisp_api
+endif
 endif
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3036)
-PRODUCT_COPY_FILES += \
-    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:obj/lib/libisp_silicomimageisp_api.so \
-    hardware/rk29/camera/Config/libisp_silicomimageisp_api.so:system/lib/libisp_silicomimageisp_api.so 
+ifeq ($(strip $(PLATFORM_VERSION)),5.0)
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_lollipop:obj/lib/libisp_silicomimageisp_api
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_lollipop:system/lib/libisp_silicomimageisp_api
+else
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_kitkat:obj/lib/libisp_silicomimageisp_api
+PRODUCT_PACKAGES += hardware/rk29/camera/Config/libisp_silicomimageisp_api_kitkat:system/lib/libisp_silicomimageisp_api
+endif
 endif
 
 #force camera API 1
