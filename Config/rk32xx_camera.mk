c38330db4c3e6c1297e1d2fe8d091f0aebdcf2aa
dalon.zhang <dalon.zhang@rock-chips.com>
2015-03-19 09:55:24 +0800

    modify Config/rk32xx_camera.mk

--- Config/rk32xx_camera.mk
+++ Config/rk32xx_camera.mk
@@ -1,7 +1,7 @@
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)), rk3368)
 
 PRODUCT_PACKAGES += \
-    #libisp_silicomimageisp_api
+    libisp_silicomimageisp_api
 
 PRODUCT_COPY_FILES += \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV8820/calib/OV8820.xml:system/etc/OV8820.xml \
@@ -14,8 +14,10 @@ PRODUCT_COPY_FILES += \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV8858/calib/OV8858_lens_SUNNY-3813A_OTP.xml:system/etc/OV8858_lens_SUNNY-3813A_OTP.xml \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV5648/calib/OV5648_lens_CHT-842B-MD_OTP.xml:system/etc/OV5648_lens_CHT-842B-MD_OTP.xml \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV13850/calib/OV13850.xml:system/etc/OV13850.xml \
+	hardware/rockchip/camera/SiliconImage/isi/drv/GS8604/calib/GS8604_lens_9569A2.xml:system/etc/GS8604_lens_9569A2.xml \
+	hardware/rockchip/camera/SiliconImage/isi/drv/GS8604/calib/GS8604_lens_40100A.xml:system/etc/GS8604_lens_40100A.xml \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV2680/calib/OV2680.xml:system/etc/OV2680.xml \
-    hardware/rockchip/camera/Config/cam_board.xml:system/etc/cam_board.xml 
+    hardware/rockchip/camera/Config/cam_board_rk3368.xml:system/etc/cam_board.xml
 	
 endif
 
@@ -35,8 +37,11 @@ PRODUCT_COPY_FILES += \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV8858/calib/OV8858_lens_SUNNY-3813A_OTP.xml:system/etc/OV8858_lens_SUNNY-3813A_OTP.xml \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV5648/calib/OV5648_lens_CHT-842B-MD_OTP.xml:system/etc/OV5648_lens_CHT-842B-MD_OTP.xml \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV13850/calib/OV13850.xml:system/etc/OV13850.xml \
+	hardware/rockchip/camera/SiliconImage/isi/drv/GS8604/calib/GS8604_lens_9569A2.xml:system/etc/GS8604_lens_9569A2.xml \
+	hardware/rockchip/camera/SiliconImage/isi/drv/GS8604/calib/GS8604_lens_40100A.xml:system/etc/GS8604_lens_40100A.xml \
+    hardware/rockchip/camera/SiliconImage/isi/drv/IMX214/calib/IMX214_lens_50013A7_OTP.xml:system/etc/IMX214_lens_50013A7_OTP.xml \
     hardware/rockchip/camera/SiliconImage/isi/drv/OV2680/calib/OV2680.xml:system/etc/OV2680.xml \
-    hardware/rockchip/camera/Config/cam_board.xml:system/etc/cam_board.xml 
+    hardware/rockchip/camera/Config/cam_board_rk3288.xml:system/etc/cam_board.xml
 	
 endif
 
